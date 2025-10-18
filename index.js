a11fdedc679d91f831fd5cb0d4febd4ce24b5e12
lukedemarino-coder <luke.s.demarino@gmail.com>
2025-10-18 16:42:15 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const fs = require("fs");
 const axios = require("axios");
 const cheerio = require("cheerio");
+const puppeteer = require('puppeteer');
 const {
   Client,
   GatewayIntentBits,
