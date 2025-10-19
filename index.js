f1ef44b392ae905a66f84f3dec78fcf46cb7f1a9
lukedemarino-coder <luke.s.demarino@gmail.com>
2025-10-19 18:26:20 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3849,7 +3849,7 @@ async function endMatch(channel, winner, isVoided = false) {
 }
 
 // ---------------- READY ----------------
-const MAIN_GUILD_ID = "1423242905602101310";
+const MAIN_GUILD_ID = "1421221145532956722";
 
 client.once("ready", async () => {
   console.log(`✅ Logged in as ${client.user.tag}`);
